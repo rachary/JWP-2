ed5bb479f4ced10f4fc850021473e5d3823be858
rachary <53404609+rachary@users.noreply.github.com>
2023-02-10 10:57:34 +0700

    Add files via upload

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,108 +1,149 @@
 <template>
-    <footer style="width:100%;margin-top:50px;">
-        <div style="max-width:100%;width:fit-content;background:#6868AC;position:absolute;padding:20px 70px;color:white;">
-            <ul style="display:flex;flex-wrap:wrap;gap:10px;list-style:none">
-                <li style="flex:15%">
-                    <h5>FIND US</h5>
-                    <h6>Tourism Information Center</h6>
-                    <p>Jl. Gubernur Suryo 15</p>
-                    <div style="padding: 4px 8px;margin:3px 0;background:white;width:fit-content;font-weight:700;font-size:14px;border-radius:4px;">
-                        <a style="text-decoration:none;color:#6868AC;" href="">
-                        <span><fa-icon icon="fa-solid fa-phone"/></span> Kontak Kami
-                        </a>
-                    </div>
+    <footer>
+        <div class="waves">
+            <div class="wave" id="wave1"></div>
+            <div class="wave" id="wave2"></div>
+            <div class="wave" id="wave3"></div>
+            <div class="wave" id="wave4"></div>
+        </div>
+        <div class="container">
+            <div class=card>
+                <h5>Find Us</h5>
+                <p>Tourism Information Center Jl. Gubernur Suryo 15</p>
+                <nav>
+                    <a href="#"><i>Telp</i>Kontak kami</a>
+                    <a href="#"><i>FB</i></a>
+                    <a href="#"><i>IG</i></a>
+                    <a href="#"><i>Twitter</i></a>
+                </nav>
+            </div>
+            <div class="card">
+                <h5>Contact Us</h5>
+                <p>Dinas Kebudayaan, Kepemudaan dan Olahraga serta Pariwisata Kota Surabaya</p>
+                <ul>
+                    <li><i>Dot</i>Jl. Tunjungan No.1-3 Kelurahan Genteng
+                    Kecamatan Genteng, Kota Surabaya
+                    Jawa Timur, Indonesia, 60275</li>
+                    <li><i>Telp</i>031-5318409</li>
+                    <li><i>envelope</i>disbudporapar@surabaya.go.id</li>
                     
-                    <div class="social" style="font-size:28px;">
-                        <a href="#"><fa-icon icon="fa-brands fa-facebook"/></a>
-                        <a style="margin-left:10px;" href="#"><fa-icon icon="fa-brands fa-instagram"/></a>
-                    </div>
-                </li>
-                <li style="flex:35%">
-                    <h5>CONTACT US</h5>
-                    <h6>Dinas Kebudayaan, Kepemudaan dan Olahraga serta Pariwisata Kota Surabaya Kota Surabaya</h6>
-                    <div style="display:flex;flex-direction:column;align-items:flex-start;justify-content:center;text-align:left;gap:5px;">
-                        <div style="display:flex;align-items:center;justify-content:center;gap:20px;">
-                            <fa-icon class="icon" icon="fa-solid fa-location-dot"/>
-                            <p>Jl. Tunjungan No.1-3 Kelurahan Genteng Kecamatan Genteng, Kota Surabaya Jawa Timur, Indonesia, 60275</p>
-                        </div>
-                        <div style="display:flex;align-items:center;justify-content:center;gap:15px;">
-                            <fa-icon class="icon" icon="fa-solid fa-phone"/>
-                            <p>031-5318409</p>
-                        </div>
-                        <div style="display:flex;align-items:center;justify-content:center;gap:15px;">
-                            <fa-icon class="icon" icon="fa-solid fa-envelope"/>
-                            <p>disbudporapar@surabaya.go.id</p>
-                        </div>
-                    </div>
-                </li>
-                <li style="flex:20%">
-                    <h5>OUR OTHER SITES</h5>
-                    <ul class="other">
-                        <li>
-                            <a href="">Disbudporapar Surabaya</a>
-                        </li>
-                        <li>
-                            <a href="">360&deg; Surabaya</a>
-                        </li>
-                        <li>
-                            <a href="">Tiket Wisata Surabaya</a>
-                        </li>
-                        <li>
-                            <a href="">Katalog Museum Surabaya</a>
-                        </li>
-                        <li>
-                            <a href="">Bangga Surabaya</a>
-                        </li>
-                        
-                    </ul>
-                </li>
-                <li style="flex:25%">
-                    <div style="display:flex;flex-direction:column;align-items:center;">
-                        <h5>WISATA SURABAYA</h5>
-                        <img style="height:200px;" src="\public\surabaya.png" alt="">
-                    </div>
-                </li>
-                
-            </ul>
+                </ul>
+            </div>
+            <div class="card">
+                <h5>Our Other Sites</h5>
+                <ul>
+                    <li>
+                        <a href="#">Disbudporapar</a>
+                    </li>
+                    <li>
+                        <a href="#">360&deg; Surabaya</a>
+                    </li>
+                    <li>
+                        <a href="#">Tiket Wisata Surabaya</a>
+                    </li>
+                    <li>
+                        <a href="#">Katalog Museum Surabaya</a>
+                    </li>
+                    <li>
+                        <a href="#">Bangga Surabaya</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="card">
+                <h5>Wisata Surabaya</h5>
+            </div>
         </div>
     </footer>
 </template>
 
 <style scoped>
-ul li h5 {
-    font-size: 18px;
-    color: white;
-    font-weight: 300;
-    margin: 10px 0;
-}
-ul li h6 {
-    margin-top: 5px;
-    font-size: 16px;
-    font-weight: 300;
-}
-ul li p {
-    font-size: 15px;
-    margin-top: 5px;
-}
-.icon {
-    position: relative;
-    top: 3px;
-}
-.social a {
+footer {
+    position: sticky;
+    bottom: 0;
+    padding: 2rem 4rem;
+    background: #4A4A8A;
     color: white;
 }
-.social a:hover {
-    color: #FDBF4E;
+.container {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 1rem;
 }
-.other {
-    list-style: none;
+.card {
+    flex: 1;
 }
-.other li {
-    margin-top: 10px;
+.card:nth-child(1) {
+    flex: .8;
 }
-.other li a {
-    color:white;
-    text-decoration: none;
-    
+.card:nth-child(2) {
+    flex: 2;
+}
+.card h5 {
+    margin-bottom: 2rem;
+    font-size: 1.5rem;
+}
+footer .wave {
+    position: absolute;
+    top: -4.4rem;
+    left: 0;
+    width: 100%;
+    height: 4.5rem;
+    background: url(../assets/wave.png);
+    background-size: 62.5rem 6.25rem;
+}
+footer .wave#wave1 {
+    z-index: 10;
+    opacity: 1;
+    animation: animateWave 5s linear infinite;
+}
+footer .wave#wave2 {
+    z-index: 10;
+    opacity: .8;
+    top: -4rem;
+    animation: animateWave__2 4s linear infinite;
+}
+footer .wave#wave3 {
+    z-index: 10;
+    opacity: .5;
+    top: -3.5rem;
+    animation: animateWave__2 3.5s linear infinite;
+}
+footer .wave#wave4 {
+    z-index: 9;
+    opacity: .2;
+    top: -3rem;
+    animation: animateWave 3s linear infinite;
+}
+@keyframes animateWave {
+    0% {
+        background-position-x: 62.5rem;
+    }
+    100% {
+        background-position-x: 0;
+    }
+}
+@keyframes animateWave__2 {
+    0% {
+        background-position-x: 0;
+    }
+    100% {
+        background-position-x: 62.5rem
+    }
+}
+@media only screen and (max-width: 764px) {
+    footer {
+        padding: 4rem 2rem;
+    }
+    .container {
+        flex-direction: column;
+        text-align: center;
+    }
+    .card :not(h5){
+        width: 100%;
+        text-align: left;
+    }
+    .card h5 {
+        margin: 1rem 0 .5rem 0;
+    }
 }
 </style>
